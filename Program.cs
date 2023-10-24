94d4e7ce6ff229aebb9c9cbadc2e99c2e7fd379b
Ucenik <Ucenik@DESKTOP-EDUBNMJ>
2023-10-24 09:18:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3milinko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            Console.WriteLine($"prije izvrsavanja metode iznos {a}");
+            Metoda(a);
+            Console.WriteLine($"nakon izvrsavanja metode iznos {a}");
+            Console.ReadKey();
+        }
+
+        static void Metoda (int a)
+        {
+            a = 5;
+            Console.WriteLine($"tijekom izvrsavanja metode iznos {a}");
+        }
+    }
+}
